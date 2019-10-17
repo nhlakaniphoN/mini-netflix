40e9431c4b59495f86b79a950ca27d9c400ac016
Nhlakanipho Nkosi <nkosinhlakanipho3@gmail.com>
2019-10-17 05:09:54 +0200

    Removed jasmine and css for jest and scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './main/home/home.component';
+import { MoviesComponent } from './main/movies/movies.component';
+import { MylistComponent } from './main/mylist/mylist.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent },
+  {path:'movies',component:MoviesComponent },
+  {path:'mylist',component:MylistComponent },
+  {path:'',redirectTo:'/home' ,pathMatch: 'full'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MaterializeButtonModule, MaterializeCardModule } from 'materialize-angular';
+import {MatMenuModule} from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HomeComponent } from './main/home/home.component';
+import { MoviesComponent } from './main/movies/movies.component';
+import { MylistComponent } from './main/mylist/mylist.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    
+    HomeComponent,
+    MoviesComponent,
+    MylistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterializeButtonModule,
+    MaterializeCardModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
